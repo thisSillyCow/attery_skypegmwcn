55da2555e804cc3e9a34dce3b6ad248b5f531a8b
zhenHang <zhengzh@chinamobiad.com>
2018-06-13 18:35:20 +0800

    11

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/page/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="pageHome">
+         <ul class="nav nav-tabs">
+        <li role="presentation" class="active"><a href="#">Home</a></li>
+        <li role="presentation"><a href="#">Profile</a></li>
+        <li role="presentation"><a href="#">Messages</a></li>
+        </ul>
+        <theme class="theme"></theme>
+    </div>
+</template>
+
+<script>
+import theme from "../components/theme.vue";
+export default {
+    name: "index",
+    components: {
+        theme
+    }
+};
+</script>
+
+<style scoped>
+.pageHome {
+    display: flex;
+    flex-direction: column;
+}
+
+.theme {
+    position: fixed;
+    flex: 1;
+    top: 0;
+    left: 0;
+    right: 0;
+}
+</style>
